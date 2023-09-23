228aafbd696bf0aa233f858bec7a8e6cb251e55e
Michael S <tonichaelmight@gmail.com>
2023-09-22 23:51:49 -0400

    add hourly forecast

--- script.js
+++ script.js
@@ -1,25 +1,68 @@
 const main = document.getElementById('present-forecast');
+const viewSelect = document.getElementById('view-select');
 
-main.innerHTML = '';
+viewSelect.value = '7-day';
 
-const getCurrentWeatherData = async () => {
-    const response = await fetch('https://api.weather.gov/gridpoints/AKQ/44,77/forecast')
+const getSevenDayForecast = async () => {
+    main.innerHTML = '';
+    const response = await fetch('https://api.weather.gov/gridpoints/AKQ/44,77/forecast');
     const data = await response.json();
     const { periods } = data.properties;
     periods.forEach(period => {
-        let { name, startTime, endTime, isDaytime, temperature, temperatureUnit, icon, shortForecast} = period;
+        let { name, startTime, endTime, isDaytime, temperature, temperatureUnit, icon, shortForecast, detailedForecast } = period;
         startTime = new Date(startTime).toLocaleString();
         endTime = new Date(endTime).toLocaleString();
+        startTime = startTime.replace(':00:00 ', '');
+        endTime = endTime.replace(':00:00 ', '');
         icon = icon.replace('medium', 'large');
         isDaytime = isDaytime ? 'day' : 'night';
         main.innerHTML += `<div class="card ${isDaytime}">
                                 <h2>${name}</h2>
-                                <p>${startTime} &ndash; ${endTime}</p>
-                                <p>${temperature}&deg;${temperatureUnit}</p>
+                                <p class="timeframe">${startTime} &ndash; ${endTime}</p>
                                 <img src="${icon}">
-                                <p>${shortForecast}</p>
+                                <p class="temperature">${temperature}&deg;${temperatureUnit}</p>
+                                <p class="short-forecast-description">${shortForecast}</p>
+                                <details>
+                                    <summary>Select for detailed forecast</summary>
+                                    <p>${detailedForecast}</p>
+                                </details>
                             </div>`
-    })
+    });
+};
+
+const getHourlyForecast = async () => {
+    main.innerHTML = '';
+    const response = await fetch('https://api.weather.gov/gridpoints/AKQ/44,77/forecast/hourly');
+    const data = await response.json();
+    const { periods } = data.properties;
+    periods.forEach(period => {
+        let { startTime, endTime, isDaytime, temperature, temperatureUnit, icon, shortForecast} = period;
+        startTime = new Date(startTime).toLocaleString();
+        endTime = new Date(endTime).toLocaleString();
+        startTime = startTime.replace(':00:00 ', '');
+        endTime = endTime.replace(':00:00 ', '');
+        icon = icon.replace('medium', 'large');
+        isDaytime = isDaytime ? 'day' : 'night';
+        main.innerHTML += `<div class="card ${isDaytime}">
+                                <p class="timeframe">${startTime} &ndash; ${endTime}</p>
+                                <img src="${icon}">
+                                <p class="temperature">${temperature}&deg;${temperatureUnit}</p>
+                                <p class="short-forecast-description">${shortForecast}</p>
+                            </div>`
+    });
+};
+
+viewSelect.onchange = () => {
+    switch(viewSelect.value) {
+        case '7-day':
+            getSevenDayForecast();
+            break;
+        case 'hourly':
+            getHourlyForecast();
+            break;
+        default:
+            throw new Error('uh oh bestie');
+    }
 }
 
-getCurrentWeatherData();
+getSevenDayForecast();
