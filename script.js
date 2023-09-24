1010f08cc8f71592a3504e5b6eb965a5e1d5ffee
Michael S <tonichaelmight@gmail.com>
2023-09-23 21:50:21 -0400

    fix bugs

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const main = document.getElementById('present-forecast');
 const viewSelect = document.getElementById('view-select');
 
+const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
 viewSelect.value = '7-day';
 
 const getSevenDayForecast = async () => {
@@ -9,7 +11,7 @@ const getSevenDayForecast = async () => {
     const data = await response.json();
     const { periods } = data.properties;
     periods.forEach(period => {
-        let { name, startTime, endTime, isDaytime, temperature, temperatureUnit, icon, shortForecast, detailedForecast } = period;
+        let { name, startTime, endTime, isDaytime, temperature, temperatureUnit, relativeHumidity, icon, shortForecast, detailedForecast } = period;
         startTime = new Date(startTime).toLocaleString();
         endTime = new Date(endTime).toLocaleString();
         startTime = startTime.replace(':00:00 ', '');
@@ -19,8 +21,13 @@ const getSevenDayForecast = async () => {
         main.innerHTML += `<div class="card ${isDaytime}">
                                 <h2>${name}</h2>
                                 <p class="timeframe">${startTime} &ndash; ${endTime}</p>
-                                <img src="${icon}">
-                                <p class="temperature">${temperature}&deg;${temperatureUnit}</p>
+                                <div class="middle-banner">
+                                    <img src="${icon}">
+                                    <div class="stats">
+                                        <p class="temperature">${temperature}&deg;${temperatureUnit}</p>
+                                        <p class="humidity">${relativeHumidity.value}% moist</p>
+                                    </div>
+                                </div>
                                 <p class="short-forecast-description">${shortForecast}</p>
                                 <details>
                                     <summary>Select for detailed forecast</summary>
@@ -30,25 +37,32 @@ const getSevenDayForecast = async () => {
     });
 };
 
-const getHourlyForecast = async () => {
+const getSevenDayHourlyForecast = async () => {
     main.innerHTML = '';
     const response = await fetch('https://api.weather.gov/gridpoints/AKQ/44,77/forecast/hourly');
     const data = await response.json();
     const { periods } = data.properties;
     periods.forEach(period => {
-        let { startTime, endTime, isDaytime, temperature, temperatureUnit, icon, shortForecast} = period;
+        let { startTime, endTime, isDaytime, temperature, temperatureUnit, relativeHumidity, icon, shortForecast} = period;
+        const day = new Date(startTime).getDay();
         startTime = new Date(startTime).toLocaleString();
         endTime = new Date(endTime).toLocaleString();
         startTime = startTime.replace(':00:00 ', '');
         endTime = endTime.replace(':00:00 ', '');
-        icon = icon.replace('medium', 'large');
+        icon = icon.replace('small', 'medium');
         isDaytime = isDaytime ? 'day' : 'night';
         main.innerHTML += `<div class="card ${isDaytime}">
+                                <h2>${dayNames[day]}</h2>
                                 <p class="timeframe">${startTime} &ndash; ${endTime}</p>
-                                <img src="${icon}">
-                                <p class="temperature">${temperature}&deg;${temperatureUnit}</p>
+                                <div class="middle-banner">
+                                    <img src="${icon}">
+                                    <div class="stats">
+                                        <p class="temperature">${temperature}&deg;${temperatureUnit}</p>
+                                        <p class="humidity">${relativeHumidity.value}% moist</p>
+                                    </div>
+                                </div>
                                 <p class="short-forecast-description">${shortForecast}</p>
-                            </div>`
+                            </div>`;
     });
 };
 
@@ -58,7 +72,7 @@ viewSelect.onchange = () => {
             getSevenDayForecast();
             break;
         case 'hourly':
-            getHourlyForecast();
+            getSevenDayHourlyForecast();
             break;
         default:
             throw new Error('uh oh bestie');
